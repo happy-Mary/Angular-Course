4e01695bfde624b0a75172fb67d654fb1897fee3
Maria Shklovskaya <mariashklovska1991@gmail.com>
2017-02-21 16:30:28 +0000

    servises'

--- AngularMent/app/js/controllers/QuestionController.js
+++ AngularMent/app/js/controllers/QuestionController.js
@@ -1,33 +1,27 @@
-questApp.controller('QuestionController', 
-	function QuestionController($scope){
-		$scope.question={
-			text: 'Какой js-фреймворк лучше использовать?',
-			author: 'Иван Иванов',
-			date: '20/10/2013',
-			answers: 
-			[{
-				text: 'AngularJS!',
-				author: 'Вова Сидоров',
-				date: '20/10/2013',
-				rate:2
-			},{
-				text: 'AngularJS лучше всех',
-				author: 'Олег Кузнецов',
-				date: '21/10/2013',
-				rate:3
-			},{
-				text: 'фигасе квестшен',
-				author: 'Неизвестный',
-				date: '22/10/2013',
-				rate:0
-			}]
-		},
-	
-		$scope.voteUp = function (answer){
-			answer.rate++;
-		};
-		$scope.voteDown = function (answer){
-			answer.rate--;
-		};
-	}
+questApp.controller('QuestionController',
+    function QuestionController($scope, $http){
+     $scope.loaded = false;
+    	
+    $scope.load = function(){
+    	var conf = {
+    		timeout: 6000
+    	}
+    	
+    	$http.get("question.json", conf).
+    	success(function(data, status, headers, config){
+    		$scope.question = data.question;
+    		$scope.loaded = true;
+    		console.log(config);
+    	}).error(function(data, status, headers, config){
+    		console.log("Код ответа: "+ status);
+    	})
+    }
+      
+      $scope.voteUp = function (answer){
+            answer.rate++;
+        };
+      $scope.voteDown = function (answer){
+            answer.rate--;
+        };
+    }
 )
